e7e100f4318f31d6881ca8f81ba2b2ae53b1a9ef
datvo06 <datvo06@gmail.com>
2024-08-01 00:14:14 +1000

    FIX: Using split instead of StringTokenizer, also updated correct transaction count

--- test/graph_test.cpp
+++ test/graph_test.cpp
@@ -160,6 +160,7 @@ int main(int argc, char *argv[]) {
 
 #ifdef PRINT
   for (unsigned z = 0; z < level_one_pats.size(); z++) {
+    cout << "z: " << z << endl;
     cout << level_one_pats[z] << endl;
     cout << vat_map.get_vat(level_one_pats[z]) << endl;
   }
